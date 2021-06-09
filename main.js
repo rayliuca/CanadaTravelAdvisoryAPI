a7ed48db6d706dce155d0a9ad049295e1dbf9022
Ray Liu <ray@rayliu.ca>
2021-06-08 20:54:19 -0600

    Update main.js

--- main.js
+++ main.js
@@ -43,20 +43,3 @@ function save_obj(obj) {
 
 
 getJSON_vm(save_obj)
-
-const path = require('path');
-const fs = require('fs');
-//joining path of directory
-const directoryPath = path.join(__dirname, './');
-//passsing directoryPath and callback function
-fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file);
-    });
-});
